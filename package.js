70f71e65a2d9c357323582ca372a3d1882606ff4
Stephen King <stephen.brian.king@gmail.com>
2014-01-03 12:41:02 -0800

    Fix Npm.require("glob") for testing

--- package.js
+++ package.js
@@ -1,26 +1,26 @@
 /* jshint strict: false */
 
-Npm.depends({
-  "css": "1.6.0",
-  "diff": "1.0.8",
-  "glob": "3.2.7"
-});
-
-var glob = Npm.require("glob");
-
 Package.describe({
   summary: "Stylus UI framework for Meteor"
 });
 
 Package.on_use(function(api) {
   api.imply("stylus");
-
-  api.add_files(glob.sync("styles/**/*.styl"), "client");
 });
 
 Package.on_test(function(api) {
-  api.use(["tinytest", "test-helpers", "underscore", "protoplanet"]);
+  Npm.depends({
+    "css": "1.6.0",
+    "diff": "1.0.8",
+    "glob": "3.2.7"
+  });
 
-  api.add_files(glob.sync("tests/styles/**/*.@(styl|css)"), "client");
-  api.add_files(glob.sync("tests/**/*.js"), "server");
+  api.use(["tinytest", "underscore", "protoplanet"]);
+
+  var glob = Npm.require("glob");
+  if (glob) {
+    api.add_files(glob.sync("styles/**/*.styl"), "client");
+    api.add_files(glob.sync("tests/styles/**/*.@(styl|css)"), "client");
+    api.add_files(glob.sync("tests/**/*.js"), "server");
+  }
 });
