9e1a90659ab32fe6886cc2da8366d016a9add914
Stephen King <stephen.brian.king@gmail.com>
2014-01-03 22:37:47 -0800

    Commit glob package in .npm/package/node_modules

--- package.js
+++ package.js
@@ -9,20 +9,18 @@ Package.on_use(function(api) {
 });
 
 Package.on_test(function(api) {
+
   Npm.depends({
     "css": "1.6.0",
     "diff": "1.0.8",
     "glob": "3.2.7"
   });
 
+  var glob = Npm.require("glob");
+
   api.use(["tinytest", "underscore", "protoplanet"]);
 
-  var glob = Npm.require("glob");
-  if (glob) {
-    api.add_files(glob.sync("styles/**/*.styl"), "client");
-    api.add_files(glob.sync("tests/styles/**/*.@(styl|css)"), "client");
-    api.add_files(glob.sync("tests/**/*.js"), "server");
-  } else {
-    console.log("Updating NPM dependencies. Meteor may crash. Re-run the test-packages command.");
-  }
+  api.add_files(glob.sync("styles/**/*.styl"), "client");
+  api.add_files(glob.sync("tests/styles/**/*.@(styl|css)"), "client");
+  api.add_files(glob.sync("tests/**/*.js"), "server");
 });
